14812d606c9f7604b2df1f154a24e8ea44003e4b
Caio César Félix Caldeira <33360475+caio-cesarfx@users.noreply.github.com>
2018-10-18 00:13:26 -0300

    Estrutura dos vértices e das arestas

--- lista2encadeada.py
+++ lista2encadeada.py
@@ -28,19 +28,39 @@ class Aresta:
 # Arquivo .txt para guardar as informações
 # todo ler o arquivo ao inicializar o programa
 db = open("database.txt", "r+")
-db.write("Vértices\n")
+db.write("Vértices:\n")
 
 vertices = []  # Lista de vértices criados
+arestas = []
 create = True
 while create:  # Criação/ligação de vértices enquanto a função estiver ativa todo a porra do menu
     v = Vertice(input("X: "), input("Y: "))  # todo trocar input teclado por input mouse
-    if len(vertices) < 2:
+    #  Primeiro vértice
+    if not vertices:
         vertices.append(v)
-    if v in vertices:
+    #  A partir do segundo, criam-se arestas automaticamente
+    #  Se estiver voltando ao primeiro, fecha o polígono
+    elif v == vertices[0]:
         create = False
+        a = Aresta(vertices[len(vertices) - 1], vertices[0])  # Ligar o último ao primeiro
+        arestas.append(a)
         break
-    vertices.append(v)
+    # Se tentar fechar com outro vértice que não o primeiro gera erro
+    elif v in vertices and v != vertices[0]:
+        print("Erro: Não é polígono!")
+        break
+    # Caso não seja o primeiro, não retorne ao primeiro e não gere erro, cria o vértice e a aresta
+    else:
+        #  Adicionando vértices à lista
+        vertices.append(v)
+        #  Adicionando arestas à lista
+        a = Aresta(vertices[len(vertices) - 1], vertices[len(vertices) - 2])
+        arestas.append(a)
+
 
 db.write(f"{vertices}")  # Salvando no arquivo
+db.write(f"\n Arestas:\n {arestas}")
 db.close()  # Fechando o arquivo
+
 print(f"Vértices: {vertices}")  # Teste inútil
+print(f"Arestas: {arestas}")  #  Outro teste inútil
